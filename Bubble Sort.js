0e34da6dc466de54940dfc836802972a36533b37
jipunroy95 <jipunroy95@gmail.com>
2026-08-25 23:35:17 +0600

    bubble sort

--- Bubble Sort.js
+++ Bubble Sort.js
@@ -1,13 +1,23 @@
-let arr = [7, 12, 9, 11, 3];
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
 
-for (let i = 0; i < arr.length - 1; i++) {
-  for (let j = 0; j < arr.length - 1 - i; j++) {
-    if (arr[j] > arr[j + 1]) {
-      let temp = arr[j];
-      arr[j] = arr[j + 1];
-      arr[j + 1] = temp;
+            // Compare adjacent elements
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
-  }
+
+    return arr;
 }
 
-console.log(arr);
+
+let numbers = [7, 12, 9, 11, 3];
+
+let sortedNumbers = bubbleSort(numbers);
+
+console.log(sortedNumbers);
